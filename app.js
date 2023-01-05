4c8dd8f72f67e8e2bd4f70b1de88bfdfde57d8a8
yashNaiya <yashnaiya16@gmail.com>
2023-01-05 20:16:15 +0530

    commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const cookieParser = require('cookie-parser')
+const path = require('path')
+require("./Connection/connection")
+const dotenv = require('dotenv')
+
+dotenv.config({path:"././config.env"})
+
+const User = require("./Models/Users") 
+app.use(cookieParser())
+app.use(express.json())
+
+
+app.use(require('./router/auth'))
+
+app.use(express.static(path.join(__dirname,'./client/build')))
+
+app.get("*",function(req,res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Be Started at port ${process.env.PORT}`)
+ })
