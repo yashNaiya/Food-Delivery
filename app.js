4c726452f907af408ab1f2cc7625996c288d8f6b
Yash Naiya <79757469+yashNaiya@users.noreply.github.com>
2023-01-10 19:16:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,14 +13,25 @@ app.use(cors({Credential:true,
               methods:"GET,HEAD,PUT,PATCH,POST,DELETE",
               preflightContinue:false}));
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://strong-pavlova-dad727.netlify.app/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
     next();
-  });
+});
 
 const User = require("./Models/Users")
 app.use(cookieParser())
