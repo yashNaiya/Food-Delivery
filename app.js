dc4d74dc331ebb9924a8f96e01d81aba58999029
yashNaiya <yashnaiya16@gmail.com>
2023-01-11 19:47:45 +0530

    commit

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ const PORT = process.env.PORT || 9002
 dotenv.config({ path: "././config.env" })
 
 var cors = require('cors')
-app.use(cors({Credential:true,
-              origin:'*',
-              methods:"GET,HEAD,PUT,PATCH,POST,DELETE",
-              preflightContinue:false}));
+app.use(cors({
+    Credential: true,
+    origin: '*',
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false
+}));
 
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
@@ -23,7 +25,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
@@ -33,25 +35,7 @@ app.use(function (req, res, next) {
     next();
 });
 
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 
 const User = require("./Models/Users")
 app.use(cookieParser())
