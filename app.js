c801bbeadaf91b1afd685eb55a71de057c27907c
yashNaiya <yashnaiya16@gmail.com>
2023-01-12 18:53:11 +0530

    commit

--- app.js
+++ app.js
@@ -9,25 +9,28 @@ dotenv.config({ path: "././config.env" })
 
 var cors = require('cors')
 app.use(cors({
+    Credential: true,
+    origin: "https://63bee239ae65460ef18247ea--strong-pavlova-dad727.netlify.app",
+    methods: ["GET", "POST"],
     preflightContinue: true,
 }));
 
 
 // Add headers before the routes are defined
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
 
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'https://63bee239ae65460ef18247ea--strong-pavlova-dad727.netlify.app');
 
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
     next();
