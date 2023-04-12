6ec67674353ec7b8b5dc8c560a5758b0b1eb8ed0
yashNaiya <yashnaiya16@gmail.com>
2023-04-12 13:07:15 +0530

    commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 9002
 dotenv.config({ path: "././config.env" })
 //hello from kashyap
 var cors = require('cors')
-const origin = ["https://strong-pavlova-dad727.netlify.app", "http://localhost:3000"]
+const origin = ["https://thebombay.netlify.app", "http://localhost:3000"]
 app.use(cors({
     credentials: true,
     origin: origin,
